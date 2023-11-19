e37b5b016c868e80268b779c773a2c404e533b8c
timconceivable <headskape@yahoo.com>
2023-11-19 14:27:45 -0600

    optional styling and commenting out packinglock.json in gitignore file

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,5 @@
+import "./styling.css"
+
 export default function Banner () {
    return (
       <>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,9 @@
 import satData from "./satData";
+import "./styling.css"
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
+    <div className="flex-container">
       {displaySats.map((sat, id) => {
         return (
           <button onClick={() => filterByType(sat)} key={id}>
